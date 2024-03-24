d70ba9cb453c21c1f2e3f359eb7d960583249075
Lucas Iglesias <lucasiglesias2704@gmail.com>
2024-03-24 02:02:14 -0300

    gitignore e removendo numeros/strings magicos

--- app.py
+++ app.py
@@ -6,44 +6,66 @@ import os
 app = Flask(__name__)
 
 
-@app.get("/filmes/favoritos")
+rota_adicionar_aos_favoritos = "/"
+rota_obter_favoritos = "/filmes/favoritos"
+rota_obter_por_titulo = "/filmes"
+nome_arquivo_memoria = "filmes.json"
+url_omdb = 'https://www.omdbapi.com/'
+status = {'OK': 200, 'CREATED': 201, 'INTERNAL_SERVER_ERROR': 500}
+
+
+@app.get(rota_obter_favoritos)
 def obter_todos():
-    with open("filmes.json", "r") as file:
+    operacao_arquivo = "r"
+    with open(nome_arquivo_memoria, operacao_arquivo) as file:
         filmes = json.load(file)
 
     return filmes
 
 
-@app.route("/filmes", methods=['GET'])
+@app.get(rota_obter_por_titulo)
 def obter_por_titulo():
-    titulo = request.args.get('titulo', default='', type=str)
-    apiKey = os.environ['apiKey']
-    parametros = {'apiKey': apiKey, 't': titulo}
-    r = requests.get('https://www.omdbapi.com/', params=parametros)
+    parametro = 'titulo'
+    valor_padrao_parametro = ''
 
-    if r.status_code != 200:
-        return jsonify({"error": f"Erro ao acessar a API do OMDb: {r.status_code}"}), 500
+    titulo = request.args.get(
+        parametro, default=valor_padrao_parametro, type=str)
+
+    parametro_api_key = 'apiKey'
+    parametro_titulo = 't'
+    api_key = os.environ[parametro_api_key]
+
+    parametros = {parametro_api_key: api_key, parametro_titulo: titulo}
+
+    r = requests.get(url_omdb, params=parametros)
+
+    if r.status_code != status['OK']:
+        nome_erro = "error"
+        mensagem_erro = f"Erro ao acessar a API do OMDb: {r.status_code}"
+        return jsonify({f"{nome_erro}:{mensagem_erro}"}), status['INTERNAL_SERVER_ERROR']
 
     return r.json()
 
 
-@app.post("/")
+@app.post(rota_adicionar_aos_favoritos)
 def adicionar_aos_favoritos():
     filme = request.get_json()
-    filename = "filmes.json"
 
     try:
-        with open(filename, "r") as file:
+        operacao_arquivo = "r"
+        with open(nome_arquivo_memoria, operacao_arquivo) as file:
             filmes = json.load(file)
     except FileNotFoundError:
         filmes = []
 
     filmes.append(filme)
 
-    with open(filename, "w") as file:
+    operacao_arquivo = "w"
+    with open(nome_arquivo_memoria, operacao_arquivo) as file:
         json.dump(filmes, file)
 
-    return "Success", 201
+    mensagem_sucesso = "Success"
+    return mensagem_sucesso, status['CREATED']
 
 
 app.run(port=5000, host='localhost', debug=True)
