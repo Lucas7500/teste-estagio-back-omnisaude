bf035dfa28cc2123b5d3b1583f4c6fb25a28f633
Lucas Iglesias <lucasiglesias2704@gmail.com>
2024-03-23 21:21:10 -0300

    definindo a classe filme

---
+++ filme.py
@@ -0,0 +1,7 @@
+class Filme:
+    def __init__(self, titulo, ano_lancamento, classificacao, sinopse, diretor):
+        self.titulo = titulo
+        self.ano_lancamento = ano_lancamento
+        self.classificacao = classificacao
+        self.sinopse = sinopse
+        self.diretor = diretor
